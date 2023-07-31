d6e1862cb480aab7cb48a0976ad90ca8dff7dead
vre343434 <vre343434@gmail.com>
2023-07-31 17:44:23 +0800

    add character select scripts

--- Assets/ScriptableObject/CharacterSO.cs
+++ Assets/ScriptableObject/CharacterSO.cs
@@ -15,52 +15,94 @@ public class LoveCharacterData
 
     public enum SexualCharacteristics
     {
-        None,                   //無
+        無,
         S,
         M,
-        Beast,                  //獸
-        Muscle,                 //肌肉
-        Transvestite,           //偽娘
+        獸,
+        肌肉,
+        偽娘,
         AI,
-        Death_meeting,          //死會
-        Elder_brother,          //哥哥
-        Sister,                 //姊姊
-        Younger_sister,         //妹妹
-        Idol,                   //偶像
-        Marginal_seople,        //邊緣
-        Reality_enrichment,     //現充
-        Mouthless,              //無口
+        死會,
+        哥哥,
+        姊姊,
+        妹妹,
+        偶像,
+        邊緣,
+        現充,
+        無口,
         NTR,
-        Sickly,                 //病嬌
-        Genius,                 //天才
-        Two_dimensional,        //二次元
-        Poor_breasts,           //貧乳
-        Big_breasts,            //巨乳
-        Effort,                 //努力
+        病嬌,
+        天才,
+        二次元,
+        貧乳,
+        巨乳,
+        努力,
+        //None,                   //無
+        //S,                      //S
+        //M,                      //M
+        //Beast,                  //獸
+        //Muscle,                 //肌肉
+        //Transvestite,           //偽娘
+        //AI,                     //AI
+        //Death_meeting,          //死會
+        //Elder_brother,          //哥哥
+        //Sister,                 //姊姊
+        //Younger_sister,         //妹妹
+        //Idol,                   //偶像
+        //Marginal_seople,        //邊緣
+        //Reality_enrichment,     //現充
+        //Mouthless,              //無口
+        //NTR,                    //NTR
+        //Sickly,                 //病嬌
+        //Genius,                 //天才
+        //Two_dimensional,        //二次元
+        //Poor_breasts,           //貧乳
+        //Big_breasts,            //巨乳
+        //Effort,                 //努力
     }
 
     public enum SkillSpecialEffects
     {
-        none,                                                           //無,
-        buckle_blood,                                                   //扣血,
-        add_blood,                                                      //補血,
-        Replenish_blood_according_to_the_number_of_people_around,       //身旁人數補血,
-        Heals_half_damage,                                              //回复一半損傷,
-        changing_sexual_characteristics,                                //變化性徵,
-        attribute_changes,                                              //屬性改變,
-        alter_another_s_personality,                                    //改變他人性徵,
-        increased_experience,                                           //經驗增加,
-        bounce_damage_once,                                             //反彈一次傷害,
-        Immune_to_Paralysis_once,                                       //免疫一次麻痺,
-        Immune_to_skills_once,                                          //免疫一次技能,
-        Push_the_enemy_forward_one_square,                              //推敵前進一格
-        normal_damage_increased,                                        //普通傷害增加
-        The_other_party_turns_into_a_man,                               //對方性轉成男
-        paralysis,                                                      //麻痺
-        relieve_paralysis,                                              //解除麻痺
-        paralysis_and_injury,                                           //麻痺和傷害
-        harm,                                                           //傷害
-        add_one,                                                        //+1
+        無,
+        扣血,
+        補血,
+        身旁人數補血,
+        回复一半損傷,
+        變化性徵,
+        屬性改變,
+        改變他人性徵,
+        經驗增加,
+        反彈一次傷害,
+        免疫一次麻痺,
+        免疫一次技能,
+        推敵前進一格,
+        普通傷害增加,
+        對方性轉成男,
+        麻痺,
+        解除麻痺,
+        麻痺和傷害,
+        傷害,
+        add_1,
+        // none,                                                           //無,
+        // buckle_blood,                                                   //扣血,
+        // add_blood,                                                      //補血,
+        // Replenish_blood_according_to_the_number_of_people_around,       //身旁人數補血,
+        // Heals_half_damage,                                              //回复一半損傷,
+        // changing_sexual_characteristics,                                //變化性徵,
+        // attribute_changes,                                              //屬性改變,
+        // alter_another_s_personality,                                    //改變他人性徵,
+        // increased_experience,                                           //經驗增加,
+        // bounce_damage_once,                                             //反彈一次傷害,
+        // Immune_to_Paralysis_once,                                       //免疫一次麻痺,
+        // Immune_to_skills_once,                                          //免疫一次技能,
+        // Push_the_enemy_forward_one_square,                              //推敵前進一格
+        // normal_damage_increased,                                        //普通傷害增加
+        // The_other_party_turns_into_a_man,                               //對方性轉成男
+        // paralysis,                                                      //麻痺
+        // relieve_paralysis,                                              //解除麻痺
+        // paralysis_and_injury,                                           //麻痺和傷害
+        // harm,                                                           //傷害
+        // add_one,                                                        //+1
     }
 
     public enum SkillDirection
@@ -73,34 +115,62 @@ public class LoveCharacterData
 
     public enum LoveSkill
     {
-        man_i_m_coming_in,                              //小夫我要進來了,
-        Listen_and_let_me_see,                          //聽話讓我看看,
-        Can_t_flirt,                                    //不可以色色,
-        undress,                                        //脫衣,
-        so_cute_must_be_a_boy,                          //這麼可愛一定是男孩子,
-        Aaaaaaaaaaaaaaah,                               //阿阿阿阿阿阿阿阿阿,
-        wink,                                           //wink,
-        evil_smile,                                     //邪笑,
-        Debug,                                          //Debug,
-        what_about_liars,                               //騙子又怎樣呢,
-        I_was_a_doctor_in_my_previous_life,             //我前世是醫生,
-        Performance,                                    //表演,
-        with_acting,                                    //配合演技,
-        Annual_salary_of_100_million_passive_income,    //年薪一億，被動收入,
-        guitar_hero,                                    //吉他英雄,
-        group,                                          //組團,
-        Sing,                                           //唱歌,
-        eat_grass,                                      //吃草,
-        guillotine,                                     //斷頭台,
-        loving_cuisine,                                 //愛心料理,
-        gaze,                                           //凝視,
-        c8736,                                          //c8736,
-        Confess_first_and_lose,                         //先告白就輸了,
-        tease,                                          //捉弄,
-        slow,                                           //遲鈍,
-        shemale_boxing,                                 //人妖拳法,
-        hormone_fruit,                                  //賀爾蒙果實,
-        invitation_sword,                               //邀請之劍,
+        小夫我要進來了,
+        聽話讓我看看,
+        不可以色色,
+        脫衣,
+        這麼可愛一定是男孩子,
+        阿阿阿阿阿阿阿阿阿,
+        wink,
+        邪笑,
+        Debug,
+        騙子又怎樣呢,
+        我前世是醫生,
+        表演,
+        配合演技,
+        年薪一億_被動收入,
+        吉他英雄,
+        組團,
+        唱歌,
+        吃草,
+        斷頭台,
+        愛心料理,
+        凝視,
+        c8736,
+        先告白就輸了,
+        捉弄,
+        遲鈍,
+        人妖拳法,
+        賀爾蒙果實,
+        邀請之劍,
+        //man_i_m_coming_in,                              //小夫我要進來了,
+        //Listen_and_let_me_see,                          //聽話讓我看看,
+        //Can_t_flirt,                                    //不可以色色,
+        //undress,                                        //脫衣,
+        //so_cute_must_be_a_boy,                          //這麼可愛一定是男孩子,
+        //Aaaaaaaaaaaaaaah,                               //阿阿阿阿阿阿阿阿阿,
+        //wink,                                           //wink,
+        //evil_smile,                                     //邪笑,
+        //Debug,                                          //Debug,
+        //what_about_liars,                               //騙子又怎樣呢,
+        //I_was_a_doctor_in_my_previous_life,             //我前世是醫生,
+        //Performance,                                    //表演,
+        //with_acting,                                    //配合演技,
+        //Annual_salary_of_100_million_passive_income,    //年薪一億，被動收入,
+        //guitar_hero,                                    //吉他英雄,
+        //group,                                          //組團,
+        //Sing,                                           //唱歌,
+        //eat_grass,                                      //吃草,
+        //guillotine,                                     //斷頭台,
+        //loving_cuisine,                                 //愛心料理,
+        //gaze,                                           //凝視,
+        //c8736,                                          //c8736,
+        //Confess_first_and_lose,                         //先告白就輸了,
+        //tease,                                          //捉弄,
+        //slow,                                           //遲鈍,
+        //shemale_boxing,                                 //人妖拳法,
+        //hormone_fruit,                                  //賀爾蒙果實,
+        //invitation_sword,                               //邀請之劍,
     }
 
     public enum LoveCharaName
