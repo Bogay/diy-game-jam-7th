0003d53011b3092525f61611686c0569f7905fae
bogay <pojay11523@gmail.com>
2023-08-06 16:18:54 +0800

    feat(skill): add: Guitar Hero

--- Assets/Scripts/Model/Skills/SkillRange.cs
+++ Assets/Scripts/Model/Skills/SkillRange.cs
@@ -21,19 +21,41 @@ public class SkillRange
         switch (this.Direction)
         {
             case SkillDirection.Forward:
-                (int, int) cursor = source;
-                for (int i = 0; i < this.Distance; i++)
                 {
-                    cursor.Item1 += forward.Item1;
-                    cursor.Item2 += forward.Item2;
-                    yield return cursor;
+                    (int, int) cursor = source;
+                    for (int i = 0; i < this.Distance; i++)
+                    {
+                        cursor.Item1 += forward.Item1;
+                        cursor.Item2 += forward.Item2;
+                        yield return cursor;
+                    }
                 }
                 break;
             case SkillDirection.Self:
                 yield return source;
                 break;
             case SkillDirection.Around:
-                // TODO
+                {
+                    for (int d = 1; d <= this.Distance; d++)
+                    {
+                        (int, int) delta = (d, 0);
+                        var moves = new (int, int)[] {
+                            (-1, 1),
+                            (-1, -1),
+                            (1, -1),
+                            (1, 1),
+                        };
+                        foreach (var move in moves)
+                        {
+                            for (int i = 0; i < d; i++)
+                            {
+                                yield return (source.Item1 + delta.Item1, source.Item2 + delta.Item2);
+                                delta.Item1 += move.Item1;
+                                delta.Item2 += move.Item2;
+                            }
+                        }
+                    }
+                }
                 break;
             default:
                 yield return source;
